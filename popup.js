610ef2606a806b634315c801ae5c25833156c61c
Sly <85970858+SlyNinjutsu07@users.noreply.github.com>
2026-08-15 16:48:11 -0400

    saveTabData func

--- popup.js
+++ popup.js
@@ -1,1 +1,25 @@
 console.log("popup loaded!");
+
+async function saveTabData(){
+    try {
+        const [tab] = await chrome.tabs.query({active: true, currentWindow: true})
+
+        if (!tab){
+            console.error("no tab found")
+            return
+        }
+
+        const tabData = {
+            title: tab.title,
+            url: tab.url,
+            timeSaved: new Date().toISOString(),
+            folder: "None",
+
+        }
+
+        await chrome.storage.sync.set({[tabData.url]: tabData})
+        console.log("Tab saved successfully:", tabData)
+    } catch(error){
+        console.error("error saving tab...", error)
+    }
+}
